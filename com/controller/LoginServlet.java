26019e5c0240ec79369f77ff6273a356f601c1f3
Sneha Priya <99204106+snehapriyasinha@users.noreply.github.com>
2024-10-06 16:13:38 +0530

    Update LoginServlet.java

--- com/controller/LoginServlet.java
+++ com/controller/LoginServlet.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package controller;
+package com.controller;
 
-import dao.BookDAO;
+import com.dao.BookDAO;
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
